c01bb7af65c9806d95f57a3ebe0ff18caadd2068
Set Reyes <rey22021@byui.edu>
2023-01-07 19:03:21 -0700

    Preparation 1 code is ready!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String surname = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }
