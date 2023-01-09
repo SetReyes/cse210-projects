be58b890eb4e1060f23af953f8076f6faa4ee230
Set Reyes <rey22021@byui.edu>
2023-01-09 13:46:06 -0700

    prep 3 practice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Magic number
+        // Random randomGenerator = new Random();
+        // int magic = randomGenerator.Next(1, 100);
+
+        // Console.Write("What is the magic number? "); string magicNumber = Console.ReadLine(); int magic = int.Parse(magicNumber);
+
+
+
+        // Declaring variables
+        int attempts = 1;
+        int magic = 0;
+        int guess = 1;
+
+        // Compare magic number with guess number
+        string userInput = "yes";
+
+        do
+        {
+            // Magic number
+            Random randomGenerator = new Random();
+            magic = randomGenerator.Next(1, 100);
+
+
+            do
+            {
+                // Request for a guess
+                Console.Write("What is your guess? ");
+                string guessNumber = Console.ReadLine();
+                guess = int.Parse(guessNumber);
+
+                // Applying logic to guess magic number
+                if (magic > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magic < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                // Counting attempts
+                attempts += 1;
+
+            } while (magic != guess);
+
+            Console.WriteLine($"You guessed it after {attempts} attempts!");
+            Console.WriteLine("Do you want to continue? ");
+            userInput = Console.ReadLine();
+            attempts = 0;
+
+
+        } while (userInput == "yes");
     }
 }
