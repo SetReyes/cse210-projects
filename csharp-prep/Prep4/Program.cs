6b1dfbab52b73ac53d20b05ce7451739ddccc9a3
Set Reyes <rey22021@byui.edu>
2023-01-09 19:15:31 -0700

    Prep 4 - LoopsPrep 4 - Loops

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Declaring variables
+        int number = -1;
+        List<int> numbers = new List<int>();
+        List<int> positive = new List<int>();
+
+        // Instructions
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        // Loop to add numbers
+        do
+        {   // Converting each number into an integer
+            Console.Write("Enter number: ");
+            string inputNumber = Console.ReadLine();
+            number = int.Parse(inputNumber);
+
+            // Appending number to the lists
+            if (number != 0)
+                numbers.Add(number);
+            if (number > 0)
+                positive.Add(number);
+
+        } while (number != 0);
+
+        // Adding list's numbers:
+        int result = numbers.AsQueryable().Sum();
+
+        // Getting list's average number:
+        decimal countItems = numbers.Count();
+        decimal aveNumber = (result / countItems);
+
+        //List's max & min pos (two ways)
+        int minPos = positive.AsQueryable().Min();
+        int max = numbers.Max();
+
+        // Sorted list
+        numbers.Sort();
+
+        // Presenting Results:
+        Console.WriteLine("The sum is: {0}", result);
+        Console.WriteLine(String.Format("The average is: {0:0.###}", aveNumber));
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest positive number is: {minPos}");
+        Console.WriteLine($"The sorted list is:");
+        foreach (int ele in numbers)
+        {
+            Console.WriteLine(ele);
+        }
+
     }
 }
