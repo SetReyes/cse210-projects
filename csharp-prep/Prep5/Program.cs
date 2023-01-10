f2a3e29b768a3a83880794c8d68c6dbbd5945d11
Set Reyes <rey22021@byui.edu>
2023-01-10 01:13:20 -0700

    Prep 5 - Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favNumber = Console.ReadLine();
+        int number = int.Parse(favNumber);
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int sq = number * number;
+        return sq;
+    }
+    static void DisplayResult(string name, int sq)
+    {
+        Console.WriteLine($"{name}, the square of your number is {sq}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
     }
+
 }
